ef8be91d03483102d63c5bd149b14eb6d7c33198
Alexandr Davlatov <ADavlatov@alfabank.ru>
2024-11-23 23:25:11 +0300

    Add tasks by education in Golang

--- Tsks/create_string.go
+++ Tsks/create_string.go
@@ -4,11 +4,31 @@ import (
 	"fmt"
 )
 
+func getUserInput() (string, string, string) {
+	var name, city, hobby string
+	
+	fmt.Print("Enter name: ")
+	fmt.Scanln(&name)
+	
+	fmt.Print("Enter city: ")
+	fmt.Scanln(&city)
+	
+	fmt.Print("Enter hobby: ")
+	fmt.Scanln(&hobby)
+
+	return name, city, hobby
+}
+
 func stringCreate(name string, city string, hobby string) string {
-	return fmt.Sprintf("Hi! My name %s . I'm from %s . My hobby is %s", name, city, hobby)
+	return fmt.Sprintf("Hi! My name %s. I'm from %s. My hobby is %s.", name, city, hobby)
 }
 
 func main() {
-	message := stringCreate("Jack", "New York", "kvadrobing")
-	fmt.Println(message)
+	name, city, hobby := getUserInput()
+
+	message1 := stringCreate("Jack", "New York", "kvadrobing")
+	fmt.Println("Old version without user input: ", message1)
+
+	message2 := stringCreate(name, city, hobby)
+	fmt.Println("New version: ", message2)
 }
